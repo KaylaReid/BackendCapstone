1d23a102cf6152e2df34e62fe44f7eb310f79ece
Kayla Reid <iamkaylareid@gmail.com>
2019-01-02 13:24:20 -0600

    added bootstrap4

--- travoul/Controllers/HomeController.cs
+++ travoul/Controllers/HomeController.cs
@@ -3,16 +3,37 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using travoul.Data;
 using travoul.Models;
 
 namespace travoul.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public HomeController(ApplicationDbContext ctx,
+                          UserManager<ApplicationUser> userManager)
         {
-            return View();
+            _userManager = userManager;
+            _context = ctx;
+        }
+
+        private Task<ApplicationUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
+
+        [Authorize]
+        public async Task<IActionResult> Index()
+        {
+            //if (await GetCurrentUserAsync() == null)
+            //{
+            //    RedirectToAction("Account/Login", "Identity");
+            //}
+                return View();
         }
 
         public IActionResult About()
