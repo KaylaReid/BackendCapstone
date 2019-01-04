66cf9da5e8d253fb5974ac85bc7c03e33f45eaf3
Kayla Reid <iamkaylareid@gmail.com>
2019-01-04 17:38:28 -0600

    styled plannedTripEdit

--- travoul/wwwroot/js/EditPlannedTrip.js
+++ travoul/wwwroot/js/EditPlannedTrip.js
@@ -1,87 +1,107 @@
-﻿console.log("Dope")
+﻿const locationContainer = document.querySelector(".plan-create-locations-card");
 
-const locationContainer = document.querySelector("#all-locations-container");
+let foodI = document.querySelector(".food-container").children.length;
 
-let foodIncrementer = document.querySelector("#food-container").children.length;
-console.log(foodIncrementer)
-let visitIncrementer = document.querySelector("#visit-container").children.length;
-console.log(visitIncrementer)
+let visitI = document.querySelector(".visit-container").children.length;
 
 locationContainer.addEventListener("click", (e) => {
     e.preventDefault();
-    if (e.target.id === "add-food-btn" || e.target.id === "add-visit-btn")
-    {
+    if (e.target.id === "add-food-btn" || e.target.id === "add-visit-btn") {
         let i = 0;
 
         let type = e.target.id.split("-")[1]
 
-        let bigType = type.charAt(0).toUpperCase() + type.slice(1);
+        let Type = type.charAt(0).toUpperCase() + type.slice(1);
 
         if (type === "food") {
-            foodIncrementer++
-            i = foodIncrementer
+            i = foodI
         } else if (type === "visit") {
-            visitIncrementer++
-            i = visitIncrementer
+            i = visitI
         }
 
-        //create parent div for new inputs, give it the proper Id
-        let newDiv = document.createElement("div")
-        newDiv.setAttribute("id", `${type}-location-${i}`)
+        let container = document.querySelector(`.${type}-container`);
 
-        //create form group containers for the input and label
-        let nameFormGroup = document.createElement("div")
-        nameFormGroup.setAttribute("class", "form-group")
-        let descFromGroup = document.createElement("div")
-        descFromGroup.setAttribute("class", "form-group")
+        let newParent = document.createElement("div");
+        newParent.setAttribute("class", "card p-2 bg-light mb-2");
+        newParent.setAttribute("id", `${type}-location-${i + 1}`);
 
-        //create input and label for name and give it the required attributes
-        let nameInput = document.createElement("input")
-        let nameLabel = document.createElement("label")
-        nameLabel.textContent = "Name"
-        nameLabel.setAttribute("for", `${type}-name-${i}`)
-        nameLabel.setAttribute("class", "control-label")
-        nameInput.setAttribute("Placeholder", "Ex. Hattie B's")
-        nameInput.setAttribute("class", "form-control")
-        nameInput.setAttribute("id", `${type}-name-${i}`)
-        nameInput.setAttribute("data-val", true)
-        nameInput.setAttribute("data-val-required", "The Name field is required")
-        nameInput.setAttribute("name", `New${bigType}Locations[${i - 1}].Name`)
+        let innerDiv = document.createElement("div");
+        let buttonDiv = document.createElement("div");
+        buttonDiv.setAttribute("class", "d-flex justify-content-end");
 
-        // create input and label for description and give it required attributes
-        let descriptionInput = document.createElement("input")
-        let descriptionLabel = document.createElement("label")
-        descriptionLabel.setAttribute("for", `${type}-desc-${i}`)
-        descriptionLabel.setAttribute("class", "control-label")
-        descriptionLabel.textContent = "Description"
-        descriptionInput.setAttribute("Placeholder", "Ex. They have great hot chicken")
-        descriptionInput.setAttribute("class", "form-control")
-        descriptionInput.setAttribute("id", `${type}-desc-${i}`)
-        descriptionInput.setAttribute("data-val", true)
-        descriptionInput.setAttribute("name", `New${bigType}Locations[${i - 1}].Description`)
+        let removeBtn = document.createElement("button");
+        removeBtn.setAttribute("class", "btn btn-sm btn-danger");
+        removeBtn.setAttribute("id", `remove-${type}-btn-${i + 1}`);
+        removeBtn.textContent = "Remove";
 
-        // place name elements into form group and append to parent container 
-        nameFormGroup.appendChild(nameLabel)
-        nameFormGroup.appendChild(nameInput)
-        newDiv.appendChild(nameFormGroup)
+        buttonDiv.appendChild(removeBtn);
 
-        descFromGroup.appendChild(descriptionLabel)
-        descFromGroup.appendChild(descriptionInput)
-        newDiv.appendChild(descFromGroup)
 
-        // add a remove button
-        let removeBtn = document.createElement("button")
-        removeBtn.setAttribute("class", `btn btn-sm btn-danger`)
-        removeBtn.setAttribute("id", `remove-${type}-btn-${i}`)
-        removeBtn.textContent = "Remove"
-        newDiv.appendChild(removeBtn)
+        let formGroup1 = document.createElement("div");
+        formGroup1.setAttribute("class", "form-group");
 
-        document.querySelector(`#${type}-container`).appendChild(newDiv)
+        let formGroup2 = document.createElement("div");
+        formGroup2.setAttribute("class", "form-group");
+
+        let nameLabel = document.createElement("label");
+        nameLabel.setAttribute("for", `${Type}-name-${i + 1}`);
+        nameLabel.setAttribute("class", "control-label");
+        nameLabel.textContent = "Name";
+
+        let nameInput = document.createElement("input");
+        nameInput.setAttribute("type", "text");
+        nameInput.setAttribute("class", "form-control");
+        nameInput.setAttribute("id", `${Type}-name-${i + 1}`);
+        nameInput.setAttribute("data-val", true);
+        nameInput.setAttribute("data-val-required", "A Name is required");
+        nameInput.setAttribute("name", `New${Type}Locations[${i}].Name`);
+        if (type === "food") {
+            nameInput.setAttribute("placeholder", "Name of the Food or Eating Location")
+        } else {
+            nameInput.setAttribute("placeholder", "Name of the Place you'd like to visit")
+        }
+
+
+        let nameValidator = document.createElement("span");
+        nameValidator.setAttribute("class", "text-danger field-validation-valid");
+        nameValidator.setAttribute("data-valmsg-for", `New${Type}Locations[${i}].Name`);
+        nameValidator.setAttribute("data-valmsg-replace", true);
+
+        let descLabel = document.createElement("label");
+        descLabel.setAttribute("for", `${Type}-desc-${i + 1}`);
+        descLabel.setAttribute("class", "control-label");
+        descLabel.textContent = "Description";
+
+        let descTextarea = document.createElement("textarea");
+        descTextarea.setAttribute("type", "text");
+        descTextarea.setAttribute("class", "form-control");
+        descTextarea.setAttribute("id", `${Type}-desc-${i + 1}`);
+        descTextarea.setAttribute("name", `New${Type}Locations[${i}].Description`);
+        if (type === "food") {
+            descTextarea.setAttribute("placeholder", "(optional) Describe the Food or Eating Location")
+        } else {
+            descTextarea.setAttribute("placeholder", "(optional) Describe the Place you'd like to visit")
+        }
+
+        formGroup1.appendChild(nameLabel);
+        formGroup1.appendChild(nameInput);
+        formGroup1.appendChild(nameValidator);
+
+        formGroup2.appendChild(descLabel);
+        formGroup2.appendChild(descTextarea);
+
+        innerDiv.appendChild(formGroup1);
+        innerDiv.appendChild(formGroup2);
+
+        newParent.appendChild(innerDiv);
+        newParent.appendChild(buttonDiv);
+
+        container.appendChild(newParent);
 
         if (type === "food") {
-            return foodIncrementer
-        } else if (type === "visit") {
-            return visitIncrementer
+            return foodI++
+        } else {
+            return visitI++
         }
 
     }
@@ -89,10 +109,10 @@ locationContainer.addEventListener("click", (e) => {
     if (e.target.id.includes("remove")) {
 
         let type = e.target.id.split("-")[1];
-        let bigType = type.charAt(0).toUpperCase() + type.slice(1);
+        let Type = type.charAt(0).toUpperCase() + type.slice(1);
 
         // grab the aread to be removed and the container its being removed from
-        let removeDiv = e.target.parentElement;
+        let removeDiv = e.target.parentElement.parentElement;
         let container = removeDiv.parentElement;
 
         // remove the element
@@ -104,32 +124,28 @@ locationContainer.addEventListener("click", (e) => {
         for (let j = 0; j < i; j++) {
             let parent = container.children[j]
             parent.setAttribute("id", `${type}-location-${j + 1}`)
-            let nameLabel = container.children[j].children[0].children[0]
-            let nameInput = container.children[j].children[0].children[1]
-            nameInput.setAttribute("name", `New${bigType}Locations[${j}].Name`)
-            nameInput.setAttribute("id", `${type}-name-${j + 1}`)
-            nameLabel.setAttribute("for", `${type}-name-${j + 1}`)
-            let descLabel = container.children[j].children[1].children[0]
-            let descInput = container.children[j].children[1].children[1]
-            descInput.setAttribute("name", `New${bigType}Locations[${j}].Description`)
-            descInput.setAttribute("id", `${type}-desc-${j + 1}`)
-            descLabel.setAttribute("for", `${type}-desc-${j + 1}`)
-            let removeBtn = container.children[j].children[2]
+            let nameLabel = container.children[j].children[0].children[0].children[0]
+            let nameInput = container.children[j].children[0].children[0].children[1]
+            let nameValidator = container.children[j].children[0].children[0].children[2]
+            nameLabel.setAttribute("for", `${Type}-name-${j + 1}`)
+            nameInput.setAttribute("name", `New${Type}Locations[${j}].Name`)
+            nameInput.setAttribute("id", `${Type}-name-${j + 1}`)
+            nameValidator.setAttribute("data-valmsg-for", `NewFoodLocations[${j}].Name`)
+            let descLabel = container.children[j].children[0].children[1].children[0]
+            let descTextarea = container.children[j].children[0].children[1].children[1]
+            descLabel.setAttribute("for", `${Type}-desc-${j + 1}`)
+            descTextarea.setAttribute("name", `New${Type}Locations[${j}].Description`)
+            descTextarea.setAttribute("id", `${Type}-desc-${j + 1}`)
+            let removeBtn = container.children[j].children[1].children[0]
             removeBtn.setAttribute("id", `remove-${type}-btn-${j + 1}`)
         }
 
         if (type === "food") {
-
-            foodIncrementer = i;
-            return foodIncrementer
-
+            foodI = i;
+            return foodI
         } else if (type === "visit") {
-
-            visitIncrementer = i;
-            return visitIncrementer
-
+            visitI = i;
+            return visitI
         }
-
     }
-
 })
